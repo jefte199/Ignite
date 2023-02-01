23244c3b3b90590e20f10235469f1df7930cb9be
Jefte <jefte199@gmail.com>
2023-02-01 10:24:31 -0300

    feat: function writable and transform

--- introduction/streams/fundamentals.js
+++ introduction/streams/fundamentals.js
@@ -1,7 +1,7 @@
 process.stdin
   .pipe(process.stdout)
 
-import { Readable } from 'node:stream';
+import { Readable, Writable, Transform } from 'node:stream';
 
 class OneTohundredStream extends Readable {
   index = 1;
@@ -9,14 +9,31 @@ class OneTohundredStream extends Readable {
     const i = this.index++
 
     setTimeout(() => {
-      if(i >= 100) {
+      if (i >= 100) {
         this.push(null)
       } else {
-        const buffer = Buffer.from(String(i)) 
+        const buffer = Buffer.from(String(i))
         this.push(buffer)
-      }  
+      }
     }, 100)
-  } 
+  }
 }
 
-new OneTohundredStream().pipe(process.stdout)
+class InverseNumberStream extends Transform {
+  _transform(chunck, encoding, callback) {
+    const transform = Number(chunck.toString()) * -1;
+    callback(null, Buffer.from(String(transform)));
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback()
+  }
+}
+
+
+new OneTohundredStream()
+.pipe(new InverseNumberStream())
+.pipe(new MultiplyByTenStream())

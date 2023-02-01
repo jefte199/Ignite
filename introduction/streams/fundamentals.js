96cd2865341577724783d7cf2f740735fa99c3cf
Jefte <jefte199@gmail.com>
2023-02-01 09:53:08 -0300

    feat: function stream

--- introduction/streams/fundamentals.js
+++ introduction/streams/fundamentals.js
@@ -4,5 +4,19 @@ process.stdin
 import { Readable } from 'node:stream';
 
 class OneTohundredStream extends Readable {
-  
+  index = 1;
+  _read() {
+    const i = this.index++
+
+    setTimeout(() => {
+      if(i >= 100) {
+        this.push(null)
+      } else {
+        const buffer = Buffer.from(String(i)) 
+        this.push(buffer)
+      }  
+    }, 100)
+  } 
 }
+
+new OneTohundredStream().pipe(process.stdout)

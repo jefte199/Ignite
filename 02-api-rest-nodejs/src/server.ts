31a5f2f8c069fa21be94aa5cad5a9c273b8292ee
Jefte <jefte199@gmail.com>
2023-02-11 12:50:37 -0300

    chore: add eslint, knex and sqlite3

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,12 +1,14 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
+import { knex } from './database'
 
-const app = fastify();
+const app = fastify()
 
-app.get('/', () => {
-  return 'hellow word'
+app.get('/', async () => {
+  const get = await knex('sqlite_schema').select('*')
+  console.log(get)
+  return 'OK'
 })
 
-app.listen({ port: 3333, })
-  .then(() => {
-    console.log(`HTTP Server in port ${3333}`)
-  })
+app.listen({ port: 3333 }).then(() => {
+  console.log(`HTTP Server in port ${3333}`)
+})

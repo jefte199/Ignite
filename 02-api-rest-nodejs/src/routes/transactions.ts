25bf544bc9bd466be36f19cce575789b41a48c8c
Jefte <jefte199@gmail.com>
2023-02-27 10:00:47 -0300

    feat: create filter route

--- 02-api-rest-nodejs/src/routes/transactions.ts
+++ 02-api-rest-nodejs/src/routes/transactions.ts
@@ -4,6 +4,26 @@ import crypto from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*')
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string(),
+    })
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+    const transaction = await knex('transactions').where({ id }).first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),

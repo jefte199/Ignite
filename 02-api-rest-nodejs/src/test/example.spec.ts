ca0c3999c97ccf91e5207757871bd1737916d3a8
Jefte <jefte199@gmail.com>
2023-03-12 15:06:04 -0300

    feat: add test list transactions

--- 02-api-rest-nodejs/src/test/example.spec.ts
+++ 02-api-rest-nodejs/src/test/example.spec.ts
@@ -17,7 +17,30 @@ describe('Trajsactions routes', () => {
       amount: 5000,
       type: 'credit',
     })
-
     expect(response.statusCode).toEqual(201)
+    console.log(response.get('Set-Cookie'))
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const res = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(res.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'new transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })

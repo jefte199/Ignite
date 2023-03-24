adf1585d9cc53f5f98769b79fde4ebfaf3d1e665
Jefte <jefte199@gmail.com>
2023-03-24 10:20:12 -0300

    feat: create .env in zod

---
+++ 03-api-solid/src/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('🆘 Invalid environment variable.', _env)
+
+  throw new Error('Invalid environment variable.')
+}
+
+export const env = _env.data
--- 03-api-solid/src/server.ts
+++ 03-api-solid/src/server.ts
@@ -1,8 +1,8 @@
-import { app } from './app.';
-
+import { env } from './env'
+import { app } from './app.'
 app.listen({
   host: '0.0.0.0',
-  port: 3333,
+  port: env.PORT,
 }).then(() => {
-  console.log('🚀 HTTP Server Running!')
+  console.log(`🚀 HTTP Server Running! In port: ${env.PORT}`)
 })

304acdf8d09d8a81bfb6da0f8484f31a7f3abb71
Jefte <jefte199@gmail.com>
2023-03-31 09:54:04 -0300

    feat: use stylesheet

--- react_native/imhere/App.tsx
+++ react_native/imhere/App.tsx
@@ -1,23 +1,33 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24,
-    }}>
-      <Text style={{
-        color: '#fdfcfe', 
-        marginTop: 30, 
-        fontSize: 24,
-        fontWeight: 'bold',
-      }}>My app</Text>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        My app
+      </Text>
 
-      <Text style={{
-        color: '#6b6b6b',
-        fontSize: 16,
-      }}>Para testes de aplicação</Text>
+      <Text style={styles.eventDate}>
+        Sexta, 31 de Março de 2023 
+      </Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+  eventName: {
+    color: '#fdfcfe',
+    marginTop: 30,
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  eventDate: {
+    color: '#6b6b6b',
+    fontSize: 16,
+  },
+})

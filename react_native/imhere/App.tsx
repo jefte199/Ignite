24c1421c499a6d839f0d7e560075bd3c4e0805d8
Jefte <jefte199@gmail.com>
2023-03-30 21:20:52 -0300

    feat: learning style

--- react_native/imhere/App.tsx
+++ react_native/imhere/App.tsx
@@ -2,10 +2,22 @@ import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>My app</Text>
-      <Text>My app</Text>
-      <Text>My app</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24,
+    }}>
+      <Text style={{
+        color: '#fdfcfe', 
+        marginTop: 30, 
+        fontSize: 24,
+        fontWeight: 'bold',
+      }}>My app</Text>
+
+      <Text style={{
+        color: '#6b6b6b',
+        fontSize: 16,
+      }}>Para testes de aplicação</Text>
     </View>
   )
 }

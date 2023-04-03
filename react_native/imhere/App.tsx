339d54fc789cf9ad576986a53d70dabe4a4f457a
Jefte <jefte199@gmail.com>
2023-04-03 10:21:34 -0300

    feat: add statusbar

--- react_native/imhere/App.tsx
+++ react_native/imhere/App.tsx
@@ -1,7 +1,11 @@
+import { StatusBar } from 'react-native';
 import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <Home />
+    <>
+      <StatusBar />
+      <Home />
+    </>
   );
 }

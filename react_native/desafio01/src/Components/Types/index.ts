921cf729a2a1faabce5483c6808db4d2a9279355
Jefte <jefte199@gmail.com>
2023-04-09 00:06:38 -0300

    fix: remove type

--- react_native/desafio01/src/Components/Types/index.ts
+++ react_native/desafio01/src/Components/Types/index.ts
@@ -1,7 +1,6 @@
 export interface task {
   tasks: string;
   removeTask: () => void;
-  preCount: () => void;
   CountCheck: number;
   setCountCheck: React.Dispatch<React.SetStateAction<number>>;
 }

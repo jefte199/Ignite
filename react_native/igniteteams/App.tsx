ca57b5c78bc48059681acb5dd3f968f306ea2d0e
Jefte <jefte199@gmail.com>
2023-04-10 20:12:00 -0300

    init new projetc

---
+++ react_native/igniteteams/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { Group } from './src/screens/Groups';
+
+export default function App() {
+  return (
+    <>
+      <Group />
+      <StatusBar style="auto" />
+    </>
+  );
+}
---
+++ react_native/igniteteams/src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export function Group() {
+  return (
+    <View style={styles.container}>
+      <Text>Group</Text>
+    </View>
+  );
+}

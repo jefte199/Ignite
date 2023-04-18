66c0bea80707efea29a7ff014df783ef45322d58
Jefte <jefte199@gmail.com>
2023-04-18 09:01:07 -0300

    feat: create styled types

---
+++ react_native/igniteteams/src/types/styled.d.ts
@@ -0,0 +1,8 @@
+import "styled-components";
+import themes from "../themes";
+
+declare module 'styled-components' {
+  type ThemeType = typeof themes;
+
+  export interface DefaultTheme extends ThemeType { }
+}

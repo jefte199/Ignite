01119b7d5667352d975c25c7480960f406359712
Jefte <jefte199@gmail.com>
2023-04-22 17:00:01 -0300

    feat: create component list empty

--- react_native/igniteteams/src/screens/Groups/index.tsx
+++ react_native/igniteteams/src/screens/Groups/index.tsx
@@ -4,26 +4,29 @@ import { Container, } from './styles';
 import { Header } from '@components/Header';
 import { Highlith } from '@components/Highlight/inde';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Group() {
-  const [group, setGroup] = useState(['Jogador']);
+  const [groups, setGroups] = useState([]);
 
   return (
     <Container>
       <Header showBackButton={false} />
 
       <Highlith
-        title='Turmas'
-        subtitle='Jogue com sua turma' />
+        title="Turmas"
+        subtitle="Jogue com sua turma" />
 
       <FlatList
-        data={group}
+        data={groups}
         keyExtractor={item => item}
         renderItem={({ item }) => (
           <GroupCard
             title={item}
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message="Cadastre a primeira Turma" />}
       />
     </Container>
   );

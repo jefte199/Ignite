6d42239f78e681262ea7f1356ac880f5a5b49d9d
Jefte <jefte199@gmail.com>
2023-04-24 09:41:52 -0300

    feat: create input component

---
+++ react_native/igniteteams/src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import { TextInputProps } from "react-native";
+import { Container } from "./styles";
+
+export function Input({ ...rest }: TextInputProps) {
+  return (
+    <Container {...rest} />
+  )
+}

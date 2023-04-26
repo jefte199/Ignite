c333dea72c4ba7086cb9e02d112af571b145aa32
Jefte <jefte199@gmail.com>
2023-04-26 10:34:27 -0300

    feat: create filter component

--- react_native/igniteteams/src/components/ButtonIcon/index.tsx
+++ react_native/igniteteams/src/components/ButtonIcon/index.tsx
@@ -1,16 +1,17 @@
 import { TouchableOpacityProps } from 'react-native';
-import { Container, Icon } from './styles';
+import { Container, Icon, ButtonIconTypeStyleProps } from './styles';
 
 type Props = TouchableOpacityProps & {
-
+  icon: string;
+  type: ButtonIconTypeStyleProps;
 }
 
-export function ButtonIcon({}: Props){
+export function ButtonIcon({ icon, type }: Props){
   return (
     <Container>
       <Icon 
-      name="home"
-      type="PRIMARY" />
+      name={icon}
+      type={type} />
     </Container>
   )
 }
--- react_native/igniteteams/src/screens/Players/index.tsx
+++ react_native/igniteteams/src/screens/Players/index.tsx
@@ -1,7 +1,8 @@
-import { Container } from './styles';
+import { Container, Form } from './styles';
 import { Header } from '@components/Header';
 import { Highlith } from '@components/Highlight/inde';
 import { ButtonIcon } from '@components/ButtonIcon';
+import { Input } from '@components/Input';
 
 export function Players() {
   return (
@@ -12,7 +13,16 @@ export function Players() {
         title='Nome da turma'
         subtitle='Adicione uma turma' />
 
-      <ButtonIcon />
+      <Form>
+        <Input
+          placeholder='Nome da turma'
+          autoCorrect={false} />
+
+        <ButtonIcon
+          icon='add'
+          type='PRIMARY'
+        />
+      </Form>
     </Container>
   )
 }

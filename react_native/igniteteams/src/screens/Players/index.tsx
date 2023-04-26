82151282b533b78bdca94a86b40f6fbbdb2911ce
Jefte <jefte199@gmail.com>
2023-04-26 09:53:20 -0300

    feat: create buttonIcon component

--- react_native/igniteteams/src/screens/Players/index.tsx
+++ react_native/igniteteams/src/screens/Players/index.tsx
@@ -1,16 +1,18 @@
 import { Container } from './styles';
 import { Header } from '@components/Header';
 import { Highlith } from '@components/Highlight/inde';
+import { ButtonIcon } from '@components/ButtonIcon';
 
-export function Players () {
+export function Players() {
   return (
     <Container>
       <Header showBackButton={true} />
 
       <Highlith
-      title='Nome da turma'
-      subtitle='Adicione uma turma' />
+        title='Nome da turma'
+        subtitle='Adicione uma turma' />
 
+      <ButtonIcon />
     </Container>
   )
 }

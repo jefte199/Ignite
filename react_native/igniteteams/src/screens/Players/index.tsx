4cec86285fe1494da9a3ed3cd5b93a3536c1d91a
Jefte <jefte199@gmail.com>
2023-05-15 10:31:05 -0300

    feat: create player card component

--- react_native/igniteteams/src/screens/Players/index.tsx
+++ react_native/igniteteams/src/screens/Players/index.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
+
 import { Container, Form, HeaderList, NumbersOfPlayers } from './styles';
+
 import { Header } from '@components/Header';
 import { Highlith } from '@components/Highlight/inde';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
+import { PlayerCard } from '@components/PlayerCard';
+
 export function Players() {
   const [team, setTeam] = useState("Time A")
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState(['Jefté'])
   return (
     <Container>
       <Header showBackButton={true} />
@@ -44,6 +48,17 @@ export function Players() {
           {players.length}
         </NumbersOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard 
+          name={item} 
+          onRemove={() => { }}
+          />
+        )}
+      />
     </Container>
   )
 }

a5f0070a495ed8c651f22a8cf1d1561b03e9aefd
Jefte <jefte199@gmail.com>
2023-05-20 18:07:06 -0300

    feat: navigation

--- react_native/igniteteams/App.tsx
+++ react_native/igniteteams/App.tsx
@@ -3,16 +3,14 @@ import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 import theme from './src/themes';
-//import { Groups } from '@screens/Groups';
-//import { NewGroup } from '@screens/NewGroup';
-import { Players } from '@screens/Players';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Players /> : <Loading /> }
+      {fontsLoaded ? <Routes /> : <Loading /> }
       <StatusBar 
       style="light" 
       backgroundColor="transparent"

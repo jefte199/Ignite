07d28e5c0884ff038e011edc1fba59ebe6596326
Jefte <jefte199@gmail.com>
2023-05-22 10:35:40 -0300

    feat: use react-native-safe-area-context

--- react_native/igniteteams/src/screens/NewGroup/styles.ts
+++ react_native/igniteteams/src/screens/NewGroup/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { UsersThree } from 'phosphor-react-native';
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   padding: 24px;

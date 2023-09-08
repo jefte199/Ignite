c646ec161415d2a7283540962b1875c0e19661a4
Jefte <jefte199@gmail.com>
2023-09-08 16:45:33 -0300

    feat: creates routes

--- nodejs/01-api-rest-nodejs/src/server.js
+++ nodejs/01-api-rest-nodejs/src/server.js
@@ -1,6 +1,24 @@
 import http from 'node:http';
 
+const users = [];
+
 const server = http.createServer((request, response) => {
+	const { method, url } = request;
+
+	if(method === 'GET' && url === '/users') {
+		return response
+		.setHeader('Content-type', 'application/json')
+		.end(JSON.stringify(users))
+	}
+
+	if(method === 'POST' && url === '/user') {
+		users.push({
+			name: "Jefté",
+			email: "Jefte@gmail",
+		})
+		return response.end("usuario criado")
+	}
+
 	return response.end("Welcome api");
 })
 

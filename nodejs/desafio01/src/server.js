02f8bcb2f6db3bf2a689e126f29dac3d4cab7674
Jefte <jefte199@gmail.com>
2023-09-08 18:05:15 -0300

    feat: created routes

--- nodejs/desafio01/src/server.js
+++ nodejs/desafio01/src/server.js
@@ -1,7 +1,23 @@
 import http from 'node:http';
 
+const tasks = [];
+
 const server = http.createServer((request, response) => {
 	const { method, url } = request;
+
+	if(method === 'GET' && url === '/tasks') {
+		return response
+		.setHeader('Content-type', 'application/json')
+		.end(JSON.stringify(tasks))
+	}
+
+	if(method === 'POST' && url === '/tasks') {
+		tasks.push({
+			name: "Jefté",
+			email: "Jefte@gmail",
+		})
+		return response.end("usuario criado")
+	}
 
 	return response.end("Welcome api");
 })

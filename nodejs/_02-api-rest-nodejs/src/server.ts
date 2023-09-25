017591274dbdfb09a6950a64dd1bdf6768b085a7
Jefte <jefte199@gmail.com>
2023-09-25 15:57:05 -0300

    feat: create connection knex

--- nodejs/_02-api-rest-nodejs/src/server.ts
+++ nodejs/_02-api-rest-nodejs/src/server.ts
@@ -1,9 +1,12 @@
 import  fastify from 'fastify';
+import { knex } from './database';
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'welcomew'
+app.get('/', async () => {
+  const sq = knex('sqlite_schema').select('*')
+
+  return sq
 });
 
 app.listen({

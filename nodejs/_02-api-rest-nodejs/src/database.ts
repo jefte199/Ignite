3ff952f66002773c0993754835955def4436a1b4
Jefte <jefte199@gmail.com>
2023-09-27 16:05:04 -0300

    feat: config knex migration

--- nodejs/_02-api-rest-nodejs/src/database.ts
+++ nodejs/_02-api-rest-nodejs/src/database.ts
@@ -1,9 +1,11 @@
-import { knex as setupKnex } from 'knex'
+import { knex as setupKnex, Knex } from 'knex'
 
-export const knex = setupKnex({
+export const config: Knex.Config= {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
   },
   useNullAsDefault: true,
-})
+}
+
+export const knex = setupKnex(config)

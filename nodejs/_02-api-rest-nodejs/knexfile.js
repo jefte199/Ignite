d43e0fef01678dd716e17a699f0974f15040d0a8
Jefte <jefte199@gmail.com>
2023-10-06 16:15:27 -0300

    feat: configure migrations

---
+++ nodejs/_02-api-rest-nodejs/knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './db/app.db'
+    }
+  },
+};
--- nodejs/_02-api-rest-nodejs/src/database.ts
+++ nodejs/_02-api-rest-nodejs/src/database.ts
@@ -1,9 +1,9 @@
 import { knex as setupKnex, Knex } from 'knex'
 
-export const config: Knex.Config= {
-  client: 'sqlite',
+export const config: Knex.Config = {
+  client: 'sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: './db/app.db'
   },
   useNullAsDefault: true,
 }

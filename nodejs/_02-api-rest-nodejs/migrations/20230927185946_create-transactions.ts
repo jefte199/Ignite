278612e9e6bfddace62c04deabca8fda63b9ca5e
Jefte <jefte199@gmail.com>
2023-10-17 16:13:39 -0300

    fix: add mount

--- nodejs/_02-api-rest-nodejs/migrations/20230927185946_create-transactions.ts
+++ nodejs/_02-api-rest-nodejs/migrations/20230927185946_create-transactions.ts
@@ -2,9 +2,8 @@ import { Knex } from "knex";
  
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('transactions', (table) => {
-    table.uuid('id').primary()
-    table.text('title').notNullable()
-    table.decimal('amount', 10, 2).notNullable()
+    table.uuid('id').primary(),
+    table.text('title').notNullable(),
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable
   })
 }
